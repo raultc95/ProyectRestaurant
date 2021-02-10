2435cd97dcfc35f73882e525691ddfd6d8820b31
JF <JF@LAPTOP-0EU979JV>
2021-02-10 10:21:05 +0100

    Comentario en repository

--- ProyectoRestaurante/src/Clases/Repository.java
+++ ProyectoRestaurante/src/Clases/Repository.java
@@ -7,7 +7,7 @@ public class Repository {
 	public static void main(String[] args) {
 		List<Product> carta=new ArrayList<>();
 		
-		
+		//carta
 		carta.add(new Drink("Coca-cola", 1.2, true, false));
 		System.out.println(carta);
 	}
