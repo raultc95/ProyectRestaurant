1ef30987d81c56219ce3caf5728335b08fb0f1ca
JF <JF@LAPTOP-0EU979JV>
2021-02-10 17:50:15 +0100

    Ejecutable para pruebas

---
+++ ProyectoRestaurante/src/Index.java
@@ -0,0 +1,14 @@
+import Clases.Repository;
+
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		Repository carta=new Repository();
+		carta.getAllProducts();
+			
+	}
+
+	
+
+}
