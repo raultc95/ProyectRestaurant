1ad056013341c141cb7e314ffc93b36e37e2794d
JF <JF@LAPTOP-0EU979JV>
2021-02-11 19:05:42 +0100

    Creacion de la interfaz Main menu controller

---
+++ ProyectoRestaurante/src/Interfacez/IMainMenuController.java
@@ -0,0 +1,19 @@
+package Interfacez;
+
+public interface IMainMenuController {
+	void newOrder();
+	Order changeOrder(Client c);
+	Order changeOrder(LocalDate d);
+	Order changeOrder(Client c, LocalDate d);
+	void deleteOrder(Client c);
+	void deleteOrder(LocalDate d);
+	void deleteOrder(Client c, LocalDate d);
+	Double cashToday();
+	Double cashThisMonht();
+	Double cashTotal();
+	void viewOrdersNotPayed();
+	void viewOrdersPendingDelivered();
+	void saveAllAndClose();
+
+	
+}
